d5cb4c823ef6e85b004c59ae39e38351857f8f8a
TH-DEV <th-dev@TH-DEVs-Mac-mini.local>
2024-08-07 12:21:37 +0900

    add non-comparison sort

--- README.md
+++ README.md
@@ -5,7 +5,54 @@ build suffix array
 ---
 - [x] n-grams comparison sort, by using built-in sort_by(), which takes O(N(logN)^2) time
 
-- [ ] n-grams non-comparison sort, by using Counting Sort, which takes
+- [x] n-grams non-comparison sort, by using Counting Sort, which takes
   O(NlogN) time.
 
 - [ ] SAIS
+
+---
+
+```rust
+/* simulation
+    s : "banana$"
+    sa : [0, 1, 2, 3, 4, 5, 6]
+    rank : [b, a, n, a, n, a, $]
+    pos : [0, 1, 2, 3, 4, 5, 6]
+
+    1. gap == 1
+        freq[rank[i+gap]]
+        freq : [$: 2, a: 3, n: 2]
+        running sum : [$: 2, a: 5, n: 7]
+        pos[freq[rank[i+gap]]] = i (reverse order)
+        pos : [5, 6, 0, 2, 4, 1, 3]
+
+        freq[rank[i]]
+        freq : [$: 1, a: 3, b: 1, n: 2]
+        running sum : [$:1, a: 4, b: 5, n: 7]
+        sa[freq[rank[pos[i]]] = pos[i]
+        sa : [6, 5, 1, 3, 0, 2, 4]
+
+        nrank[sa[i+1]] = nrank[sa[i]] + comp
+        nrank : [3, 2, 4, 2, 4, 1, 0]
+        rank = nrank
+
+    2. gap == 2
+        freq : [0: 3, 1: 2, 3: 2]
+        running sum : [0: 3, 1: 5, 3: 7]
+        pos[freq[rank[i+gap]]] = i
+        pos : [4, 5, 6, 1, 3, 0, 2]
+
+        freq[rank[i]]
+        freq : [0: 1, 1: 3, 2: 1, 3: 2]
+        running sum : [0: 1, 1: 4, 2: 5, 3: 7]
+        sa[freq[rank[pos[i]]]] = pos[i]
+        sa : [6, 5, 3, 1, 0, 4, 2]
+        
+        nrank[sa[i+1]] = nrank[sa[i]] + comp
+        nrank : [4, 3, 6, 2, 5, 1, 0]
+
+        rank = nrank
+        rank[sa[n-1]] == rank[2] == 6 == n-1 // break
+
+*/
+```
--- src/lib.rs
+++ src/lib.rs
@@ -1,14 +1,15 @@
 #![allow(dead_code)]
 
-fn build_sa_by_comparison(s: &str) -> Vec<usize> {
-    let n = s.len();
+fn build_sa_by_comparison(s: String) -> Vec<usize> {
+    let n = s.len() + 1;
     let mut sa = (0..n).map(|x| x).collect::<Vec<usize>>();
-    let mut rank = s
-        .as_bytes()
-        .iter()
-        .map(|&c| c as usize)
-        .collect::<Vec<usize>>();
+    let mut rank = vec![0; n];
+    s.as_bytes().iter().enumerate().for_each(|(i, &c)| {
+        rank[i] = c as usize;
+    });
+
     let mut gap = 1;
+
     while gap < n {
         let sorting = |&i: &usize, &j: &usize| {
             if rank[i] != rank[j] {
@@ -40,14 +41,101 @@ fn build_sa_by_comparison(s: &str) -> Vec<usize> {
     sa
 }
 
+fn build_sa_by_non_comparison(s: String) -> Vec<usize> {
+    let n = s.len() + 1;
+    let mut sa = (0..n).map(|x| x).collect::<Vec<usize>>();
+    let mut rank = vec![0; n];
+    s.as_bytes().iter().enumerate().for_each(|(i, &c)| {
+        rank[i] = c as usize;
+    });
+    let mut pos = vec![0; n];
+    let mut gap = 1;
+
+    while rank[sa[n - 1]] != n - 1 {
+        let mut freq = vec![0; 256];
+
+        for i in 0..n {
+            let curr_rank = rank[(i + gap).min(n - 1)];
+            freq[curr_rank] += 1;
+        }
+        for i in 0..255 {
+            freq[i + 1] += freq[i];
+        }
+        for i in (0..n).rev() {
+            let curr_rank = rank[(i + gap).min(n - 1)];
+            freq[curr_rank] -= 1;
+            pos[freq[curr_rank]] = i;
+        }
+
+        freq = vec![0; 256];
+
+        for i in 0..n {
+            freq[rank[i]] += 1;
+        }
+        for i in 0..255 {
+            freq[i + 1] += freq[i];
+        }
+        for i in (0..n).rev() {
+            let curr = rank[pos[i]];
+            freq[curr] -= 1;
+            sa[freq[curr]] = pos[i];
+        }
+
+        let sorting = |&i: &usize, &j: &usize| {
+            if rank[i] != rank[j] {
+                rank[i].cmp(&rank[j])
+            } else {
+                let i = i + gap;
+                let j = j + gap;
+                if i < n && j < n {
+                    rank[i].cmp(&rank[j])
+                } else {
+                    j.cmp(&i)
+                }
+            }
+        };
+
+        let mut nrank = vec![0; n];
+
+        for i in 0..(n - 1) {
+            let add = match sorting(&sa[i], &sa[i + 1]) {
+                std::cmp::Ordering::Less => 1,
+                _ => 0,
+            };
+            nrank[sa[i + 1]] = nrank[sa[i]] + add;
+        }
+
+        rank = nrank.clone();
+        gap <<= 1;
+    }
+
+    sa
+}
+
 #[cfg(test)]
 mod test {
     use super::*;
     #[test]
     fn test1() {
-        assert_eq!(build_sa_by_comparison("banana"), vec![5, 3, 1, 0, 4, 2]);
+        assert_eq!(
+            build_sa_by_comparison("banana".to_owned()),
+            vec![6, 5, 3, 1, 0, 4, 2]
+        );
+        assert_eq!(
+            build_sa_by_comparison("mississipi".to_owned()),
+            vec![10, 9, 7, 4, 1, 0, 8, 6, 3, 5, 2]
+        );
     }
 
     #[test]
-    fn test2() {}
+    fn test2() {
+        assert_eq!(
+            build_sa_by_non_comparison("banana".to_owned()),
+            vec![6, 5, 3, 1, 0, 4, 2]
+        );
+        assert_eq!(
+            build_sa_by_non_comparison("mississipi".to_owned()),
+            vec![10, 9, 7, 4, 1, 0, 8, 6, 3, 5, 2]
+        );
+    }
 }
